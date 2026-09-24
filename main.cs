8361c46d230021f01f61dc2d6b6df230bde00c31
Jay Lee <jay0lee@gmail.com>
2026-09-24 10:30:28 -0400

    Simplify test binary output to Hello, World!

--- main.cs
+++ main.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World! Successfully signed with Certum Cloud Code Signing!");
+        Console.WriteLine("Hello, World!");
     }
 }
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+import "fmt"
 
 func main() {
-	fmt.Printf("Hello, World! Successfully signed with Certum Cloud Code Signing on %s/%s!\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Println("Hello, World!")
 }
